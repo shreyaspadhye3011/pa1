5b474a12713d9ed78557c8a234507254c83c8bf5
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-02-05 15:47:41 -0800

    handled port in use error

--- server.js
+++ server.js
@@ -31,25 +31,25 @@ if (myArgs.length == 2) {
         fs.readFile(document_root + "/index.html", function(err, text){
         res.setHeader("Content-Type", "text/html");
             if (err) {
-                //TODO: Check err.errno and decide whether 404 or 403
-                // return res.end("404: File Not Found");
-                console.log("///"+err.errno)
+                // console.log("///"+err.errno)
                 return handleErrors(res, err.errno);
             }
+            // Success code & content
+            res.status(200);
             return res.end(text);
           });
     })
 
      // all routes
      app.get('/*', (req, res) => {
-        // res.send('Hello!')
-        console.log('*******'+req.originalUrl);
         fs.readFile(document_root + req.originalUrl, function(err, text){
         res.setHeader("Content-Type", "text/html");
             if (err) {
-                console.error(err.errno);
+                // console.error(err.errno);
                 return handleErrors(res, err.errno);
             }
+            // Success code & content
+            res.status(200);
             return res.end(text);
           });
     })
@@ -71,9 +71,8 @@ if (myArgs.length == 2) {
         return res.end(message);
     }
 
-    // TODO: handle port in use case
     // bind port & listen for connections
-    app.listen(port, () => console.log(`App listening on port ${port}!`))
+    app.listen(port, () => console.log(`App listening on port ${port}!`)).on('error', console.log("Error: Port already in use. Try a different port"));
 } else {
     console.error("Error: Please provide both the required arguments as: `node server.js <document_root> <port>`. Look at README for details")
 }
