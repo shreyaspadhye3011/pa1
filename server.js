d55fbabcc67c750824f414debdb8b6661f7b4427
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-02-05 12:59:32 -0800

    Added argument check

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
 const express = require('express')
-const siteUrl = "https://www.scu.edu/";
 const axios = require("axios");
 const cheerio = require("cheerio");
-
-// scrapper code
-const fetchData = async () => {
-  const result = await axios.get(siteUrl);
-  console.log(cheerio.load(result.data).html())
-  return cheerio.load(result.data);
-};
 const app = express()
-const port = 3000
+const siteUrl = "https://www.scu.edu/";
 
-// test setup
-app.get('/', (req, res) => res.send('Hello World!'))
+// get port and document root from command line
+const myArgs = process.argv.slice(2);
+console.log('myArgs: ', myArgs);
+// validate script arguments
+if (myArgs.length == 2) {
+    const document_root = myArgs[0]
+    const port = myArgs[1]
 
-// scrapper route
-app.get('/scrap', (req, res) => fetchData())
+    // home route
+    app.get('/', (req, res) => {
+        res.send('Hello!')
+    })
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+    // *********************** Test / experimental code ***************** //
+    // test setup
+    app.get('/test', (req, res) => res.send('Test Successful! Server responding on port ' + port))
+
+    // scrapper route
+    app.get('/scrap', (req, res) => {
+        fetchData();
+        res.end();
+    })
+
+    // scrapper code
+    const fetchData = async () => {
+        const result = await axios.get(siteUrl);
+        // console.log(cheerio.load(result.data).html())
+        return cheerio.load(result.data);
+    };
+    // *********************** Test / experimental code ***************** //
+
+    // bind port & listen for connections
+    app.listen(port, () => console.log(`App listening on port ${port}!`))
+} else {
+    console.error("Error: Please provide both the required arguments as: `node server.js <document_root> <port>`. Look at README for details")
+}
