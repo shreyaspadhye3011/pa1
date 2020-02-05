4e5b34f0e5f62da91babd8cb30553817b5746cf4
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-02-05 12:16:27 -0800

    Added html scrapping

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const siteUrl = "https://www.scu.edu/";
 const axios = require("axios");
 const cheerio = require("cheerio");
+
+// scrapper code
 const fetchData = async () => {
   const result = await axios.get(siteUrl);
-  console.log(cheerio.load(result.data).text())
+  console.log(cheerio.load(result.data).html())
   return cheerio.load(result.data);
 };
 const app = express()
@@ -13,6 +15,7 @@ const port = 3000
 // test setup
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/test', (req, res) => fetchData())
+// scrapper route
+app.get('/scrap', (req, res) => fetchData())
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
