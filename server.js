4fe1811a2935db3a8690e6df1cc44c4548892e66
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-02-05 12:13:20 -0800

    Added cheerio's scrapping

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const siteUrl = "https://www.scu.edu/";
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fetchData = async () => {
+  const result = await axios.get(siteUrl);
+  console.log(cheerio.load(result.data).text())
+  return cheerio.load(result.data);
+};
 const app = express()
 const port = 3000
 
+// test setup
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/test', (req, res) => fetchData())
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
