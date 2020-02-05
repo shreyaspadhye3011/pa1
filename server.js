0ed285040bae6a51c4c84080d8cdacd565bb443e
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-02-05 15:14:40 -0800

    handled errors and status codes

--- server.js
+++ server.js
@@ -9,17 +9,32 @@ if (myArgs.length == 2) {
     const document_root = myArgs[0]
     const port = myArgs[1]
 
+    // middleware to check parsable URI eg to handle "http://localhost/\%"
+    app.use(function(req, res, next) {
+        var err = null;
+        try {
+            decodeURIComponent(req.path)
+        } catch(e) {
+            err = e;
+        }
+        if (err){
+            // console.log(err, req.url);
+            // Respond with a 400 when decodeURIComponent fails 
+            handleErrors(res, -1);
+        }
+        next();
+    });
+
     // home route
     app.get('/', (req, res) => {
         // res.send('Hello!')
         fs.readFile(document_root + "/index.html", function(err, text){
+        res.setHeader("Content-Type", "text/html");
             if (err) {
                 // console.error(error);
                 //TODO: Check err.errno and decide whether 404 or 403
-                res.setHeader("Content-Type", "text/html");
                 return res.end("404: File Not Found");
             }
-            res.setHeader("Content-Type", "text/html");
             return res.end(text);
           });
     })
@@ -29,16 +44,33 @@ if (myArgs.length == 2) {
         // res.send('Hello!')
         console.log('*******'+req.originalUrl);
         fs.readFile(document_root + req.originalUrl, function(err, text){
+        res.setHeader("Content-Type", "text/html");
             if (err) {
-                console.error(err);
-                res.setHeader("Content-Type", "text/html");
-                return res.end("404: File Not Found");
+                console.error(err.errno);
+                return handleErrors(res, err.errno);
+                // res.status(404);
+                // return res.end("404: File Not Found");
             }
-            res.setHeader("Content-Type", "text/html");
-            res.end(text);
+            return res.end(text);
           });
-          return;
     })
+
+    function handleErrors(res, code) {
+        let statusCode = 400;
+        let message = "400: Bad Request";
+        switch(code) {
+            case -2:
+                statusCode = 404;
+                message = "404: File Not Found";
+                break;
+            case -13:
+                statusCode = 403;
+                message = "403: Insufficient permissions on the file";
+                break;
+        }
+        res.status(statusCode);
+        return res.end(message);
+    }
 
     // TODO: handle port in use case
     // bind port & listen for connections
